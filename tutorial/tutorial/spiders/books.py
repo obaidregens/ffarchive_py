98fdf903b7c9f7710a46cf33542aa6d0aa6e6b4b
fnewlkfm <fkjwqenjkfn@nmgfk4wmr.com>
2020-07-14 08:04:34 -0700

    Basic Complete ffnet Scraper with mongodb adding.

--- tutorial/tutorial/spiders/books.py
+++ tutorial/tutorial/spiders/books.py
@@ -1,14 +1,23 @@
 import scrapy
 
+import pymongo
+from bson.objectid import ObjectId
+
+mongo = pymongo.MongoClient("mongodb://localhost:27017/")
+ffn = mongo['ff_archive']['ffn']
+# ffn.delete_many({})
 
 class BooksCrawler(scrapy.Spider):
+    max_books = 1000
+    books_count = ffn.count_documents({})
     name = "books"
     start_urls = [
-        "https://www.fanfiction.net/book/Harry-Potter/?&srt=1&r=10",
+        "https://www.fanfiction.net/book/Harry-Potter/?&srt=5&r=10",
     ]
 
     def parse(self, response):
         for book in response.css('#content_wrapper_inner > div.z-list'):
+
             raw_tags = ''.join(book.css('.z-padtop2.xgray::text,.z-padtop2.xgray *::text').getall()).split(' - ')
             tags = {}
             tags['Language'] = raw_tags.pop(1)
@@ -21,6 +30,8 @@ class BooksCrawler(scrapy.Spider):
                     pass
                 elif ': ' in tag_is:
                     tag_is_arr = tag_is.split(': ')
+                    if tag_is_arr[0] in ['Chapters','Words','Reviews','Favs','Follows']:
+                        tag_is_arr[1] = int(tag_is_arr[1].replace(',',''))
                     tags[tag_is_arr[0]] = tag_is_arr[1]
                 else:
                     raw_characters = tag_is.split(']')
@@ -35,15 +46,23 @@ class BooksCrawler(scrapy.Spider):
                         else:
                             tags['Characters'] = char_set
                         tags['All Characters'] += char_set
+            # Existence Check
+            ID = int(book.css("a.stitle::attr(href)").get().split('/')[2])
+            existing = ffn.find_one({ "_id" : str(ID )})
+            if existing is not None :
+                if len(existing['Chapters']) >= book_data['Tags']['Chapters']:
+                    continue
+            else :
+                self.books_count += 1
             book_data = {
-                'ID'            : int(book.css("a.stitle::attr(href)").get().split('/')[2]),
+                '_id'           : ID,
                 'Title'         : book.css("a.stitle::text").get(),
                 'Author ID'     : int(book.css("a[href^='/u']::attr(href)").get().split('/')[2]),
-                'Author'        : book.css("a[href^='/u']::text").get(),
+                'Author Name'   : book.css("a[href^='/u']::text").get(),
                 'Description'   : book.css("div.z-indent.z-padtop::text").get(),
-                'Tags'          : tags
+                'Tags'          : tags,
+                'Chapters'      : []
             }
-
             first_time = book.css('.z-padtop2.xgray > span:nth-of-type(1)::attr(data-xutime)').get()
             second_time = book.css('.z-padtop2.xgray > span:nth-of-type(2)::attr(data-xutime)').get()
             if second_time is not None:
@@ -52,11 +71,33 @@ class BooksCrawler(scrapy.Spider):
             else:
                 book_data['Published'] = int(first_time)
 
-            yield book_data
-
-        # next_page = response.css('li.next a::attr(href)').get()
-        # if next_page is not None:
-        #     next_page = response.urljoin(next_page)
-        #     yield scrapy.Request(next_page, callback=self.parse)
+            yield scrapy.Request(
+                response.urljoin('/s/' + str(book_data['_id']) + '/1'),
+                callback = self.parseChapter,
+                cb_kwargs = {
+                    "book"          : book_data,
+                }
+            )
+        next_pg = response.xpath('//*[@id="content_wrapper_inner"]/center[1]/a[contains(text(),\'Next »\')]').attrib['href']
+        if (next_pg is not None & self.books_count <= self.max_books):
+            yield scrapy.Request(
+                response.urljoin(next_pg),
+                callback = self.parse
+            )
+    def parseChapter(self, response, book):
+        cont = ''.join(response.css('#storytext > p').getall())
+        book['Chapters'].append(cont)
+        next_btn = response.css('#chap_select + button.btn::attr(onclick)').get()
+        if (next_btn is not None):
+            yield scrapy.Request(
+                response.urljoin(next_btn.replace('self.location=\'','').rstrip('\'')),
+                callback = self.parseChapter,
+                cb_kwargs = {
+                    "book"          : book,
+                }
+            )
+        else:
+            ffn.replace_one({'_id': book['_id']}, book, True )
+            yield book
 
 # Run "scrapy crawl books -o books.json" to test
